e91230507080d1639b4178cd74941e8f9ce5da3f
Vishal Gorai <vishalgorai09@gmail.com>
2025-04-04 00:59:54 +0530

    enhanced hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  const body = document.body;
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+
+  if (menu.classList.contains('open')) {
+      body.style.overflow = 'hidden'; 
+  } else {
+      body.style.overflow = '';
+  }
+
 }
